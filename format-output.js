1a4afbafcdd8a1450539c255cb0dd4be13d2b01a
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-16 16:28:52 -0400

    Fix problem with no location

---
+++ format-output.js
@@ -0,0 +1,22 @@
+ module.exports = formatOutput
+
+ function formatOutput (data) {
+   var output = ''
+   for (var i = 0; i < 5; i++) {
+     output = output + 'Job number ' + i + ' info: \n'
+     output = output + 'name: ' + data.results[i].name + '\n'
+     if (data.results[i].locations.length === 0) {
+       output = output + 'location: none \n'
+     } else if (data.results[i].locations.length === 1) {
+       output = output + 'location: ' + data.results[i].locations[0].name + '\n'
+     } else {
+       output = output + 'locations: '
+       for (var j = 0; j < data.results[i].locations.length; j++) {
+         output = output + data.results[i].locations[j] + ' '
+       }
+     }
+     output = output + 'company: ' + data.results[i].company.name + '\n'
+     console.log(output)
+   }
+   return
+ }
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const got = require('got')
-require('dotenv').config()
+var formatOutput = require('./format-output.js')
 var jobsData
-var output = ''
 
 got('https://api-v2.themuse.com/jobs?page=1')
 .then((response) => {
   jobsData = JSON.parse(response.body)
-  for (var i = 0; i < 5; i++) {
-    output = output + 'Job number ' + i + ' info: \n'
-    output = output + 'name: ' + jobsData.results[i].name + '\n'
-    output = output + 'location: ' + jobsData.results[i].locations[0].name + '\n'
-    output = output + 'company: ' + jobsData.results[i].company.name + '\n'
-  }
-  console.log(output)
+  formatOutput(jobsData)
+  return
 })
 .catch((error) => {
   console.log(error.response.body)
+  return
 })
