764fff798b0c79f8c63c8fcd10d63f22561e9a69
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-16 12:50:28 -0400

    Pulls 5 Newest Jobs + Formats output

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const got = require('got')
 var jobsData
+var output = ''
 
 got('https://api-v2.themuse.com/jobs?page=1')
 .then((response) => {
   jobsData = JSON.parse(response.body)
-  console.log(jobsData)
-  return
+  for (var i = 0; i < 5; i++) {
+    output = output + 'Job number ' + i + ' info: \n'
+    output = output + 'name: ' + jobsData.results[i].name + '\n'
+    output = output + 'location: ' + jobsData.results[i].locations[0].name + '\n'
+    output = output + 'company: ' + jobsData.results[i].company.name + '\n'
+  }
+  console.log(output)
 })
 .catch((error) => {
   console.log(error.response.body)
-  return
 })
