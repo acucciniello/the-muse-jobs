005bc879e85d4fd0f27b3d49f89db99a324a0d6a
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-16 16:53:39 -0400

    Output sounds better finished with GeneralJobsIntent.js

--- helpers/format-output.js
+++ helpers/format-output.js
@@ -2,20 +2,21 @@
 
  function formatOutput (data, callback) {
    var output = ''
-   for (var i = 0; i < 5; i++) {
-     output = output + 'Job number ' + i + ' info: \n'
-     output = output + 'name: ' + data.results[i].name + '\n'
-     if (data.results[i].locations.length === 0) {
-       output = output + 'location: none \n'
-     } else if (data.results[i].locations.length === 1) {
-       output = output + 'location: ' + data.results[i].locations[0].name + '\n'
+   for (var i = 1; i < 6; i++) {
+     output = output + 'Job ' + i + ' info: \n'
+     output = output + 'name: ' + data.results[i - 1].name + ', \n'
+     if (data.results[i - 1].locations.length === 0) {
+       output = output + 'location: none , \n'
+     } else if (data.results[i - 1].locations.length === 1) {
+       output = output + 'location: ' + data.results[i - 1].locations[0].name + ', \n'
      } else {
        output = output + 'locations: '
-       for (var j = 0; j < data.results[i].locations.length; j++) {
-         output = output + data.results[i].locations[j] + ' '
+       for (var j = 0; j < data.results[i - 1].locations.length; j++) {
+         output = output + data.results[i - 1].locations[j] + ' '
        }
+       output = output + ', \n'
      }
-     output = output + 'company: ' + data.results[i].company.name + '\n'
+     output = output + 'company: ' + data.results[i - 1].company.name + '.\n'
    }
    callback(output)
    return
