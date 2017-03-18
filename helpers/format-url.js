2b735eace7a4e1b633458098a4d39525e3effed2
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-18 10:48:06 -0400

    Able to Specify NYC or DC Metro Areas     TODO: searching for jobs that contain commas in the name

--- helpers/format-url.js
+++ helpers/format-url.js
@@ -3,14 +3,17 @@ module.exports = formatURL
 function formatURL (type, value, callback) {
   var url = 'https://api-v2.themuse.com/jobs?page=1&api_key=' + process.env.API_KEY
   if (type === 'location') {
-    // Case where the word is separated NYC or Washington DC
-    value = value.split(' ')
-    var urlLocation = 'location='
-    for (var i = 0; i < value.length; i++) {
-      urlLocation = urlLocation + value[i] + '+'
+    var upperValue = value.toUpperCase()
+    if (upperValue === 'NEW YORK CITY METRO AREA' || upperValue === 'WASHINGTON D. C. METRO AREA') {
+      // Case where the word is separated NYC or Washington DC
+      value = value.split(' ')
+      var urlLocation = 'location='
+      for (var i = 0; i < value.length; i++) {
+        urlLocation = urlLocation + value[i] + '+'
+      }
+      urlLocation = urlLocation.substring(0, urlLocation.length - 1)
+      url = url + '&' + urlLocation
     }
-    urlLocation = urlLocation.substring(0, urlLocation.length - 1)
-    url = url + '&' + urlLocation
   }
   callback(url)
   return
