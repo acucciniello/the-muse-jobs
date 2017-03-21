92fe5047b3d99704e3e116d08dbff44553a7fba3
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-21 09:32:12 -0400

    Solve Location Handling issue, implement Alexa now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const got = require('got')
 var formatOutput = require('./format-output.js')
 var formatURL = require('./format-url.js')
 var jobsData
-var value = 'new york city metro area'
+var value = 'west valley city, ut'
 
 formatURL('location', value, function (url) {
   got(url)
